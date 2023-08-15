b25fb6119871e08fe2d785e884b302ba4b630e0a
Samuel Hall <hallsc3@cardiff.ac.uk>
2023-08-15 15:24:10 +0100

    Cardio ReadOnly Update

--- components/ExerciseCardReadOnly.js
+++ components/ExerciseCardReadOnly.js
@@ -14,6 +14,22 @@ const ExerciseCardReadOnly = ({exercise}) => {
     ));
   };
 
+  const renderCardioInputs = () => {
+    return exercise.cardioTracking.map((set, index) => (
+      <View style={{flexDirection:'row', width:'100%', justifyContent:'space-around', alignItems:'center'}}>
+        <View style={{flexDirection:'column', paddingVertical: 20}}>
+          <Text style={[globalStyles.text, {paddingVertical:10}]}>Distance: {set.distance}km</Text>
+          <Text style={globalStyles.text}>Time: {set.time}min</Text>
+        </View>
+        <View style={{flexDirection:'column',paddingVertical: 20}}>
+          <Text style={[globalStyles.text, {paddingVertical:10}]}>Speed: {set.speed}km/h</Text>
+          <Text style={globalStyles.text}>Elevation: {set.elevation}%</Text>
+        </View>
+      </View>
+    )
+    )
+  }
+
   return (
     <View
       style={{
@@ -28,11 +44,13 @@ const ExerciseCardReadOnly = ({exercise}) => {
     >
       <View style={{borderBottomWidth: 1, borderBottomColor: COLORS.tertiary, width: '100%', paddingVertical: 10}}>
         <Text style={[globalStyles.subTitle]}>{exercise.name}</Text>
+        <Text style={[globalStyles.h3, {alignSelf:'center'}]}>{exercise.type}</Text>
       </View>
       
 
       {/* Render weight and rep inputs */}
-      {renderWeightRepInputs()}
+      {exercise.type !== 'Cardio' && renderWeightRepInputs()}
+      {exercise.type === 'Cardio' && renderCardioInputs()}
     </View>
   );
 };
