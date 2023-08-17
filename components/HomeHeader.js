239f0a6622ca9a5502f8356c9cb3341d789e8f6a
Samuel Hall <hallsc3@cardiff.ac.uk>
2023-08-17 15:54:44 +0100

    Small change to HomeHeader

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -6,20 +6,12 @@ import globalStyles from '../constants/GlobalStyle';
 const HomeHeader = () => {
     const app = useApp();
 
-    if (app.currentUser.identities[0].providerType === 'anon-user') {
-        return (
-          <View>
-            <Text style={globalStyles.text}>Welcome Guest</Text>
-          </View>
-        )
-      }
-      else {
-      return (
+    return(
         <View>
           <Text style={globalStyles.text}>Welcome {app.currentUser.customData.username}</Text>
         </View>
       )
 }
-}
+
 
 export default HomeHeader
