09311fd604537c3fc68da36248b10d72c7527676
Samuel Hall <hallsc3@cardiff.ac.uk>
2023-08-17 15:53:10 +0100

    Basic JoinTribe page

--- screens/Tribes/TribeNavigator.js
+++ screens/Tribes/TribeNavigator.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import MyTribe from './MyTribe'
 import CreateTribe from './CreateTribe'
+import JoinTribe from './JoinTribe'
 
 const TribeNavigator = () => {
     const Stack = createStackNavigator()
@@ -15,7 +16,7 @@ const TribeNavigator = () => {
     >
         <Stack.Screen name='MyTribe' component={MyTribe} />
         <Stack.Screen name='CreateTribe' component={CreateTribe} />
-        {/* <Stack.Screen name='JoinTribe' component={JoinTribe} /> */}
+        <Stack.Screen name='JoinTribe' component={JoinTribe} />
     </Stack.Navigator>
   )
 }
