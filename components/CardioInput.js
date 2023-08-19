732f11b5edd0669ea60fb43cb2c8d38c21b0c154
Samuel Hall <hallsc3@cardiff.ac.uk>
2023-08-19 20:34:18 +0100

    Added icons to inputs

--- components/CardioInput.js
+++ components/CardioInput.js
@@ -2,6 +2,7 @@ import { View, Text, TextInput} from 'react-native'
 import React from 'react'
 import globalStyles from '../constants/GlobalStyle'
 import { COLORS } from '../constants/theme'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const CardioInput = ({onFocus, onDistanceChange, onSpeedChange, onTimeChange, onElevationChange}) => {
     const handleDistanceChange = (distance) => {
@@ -31,7 +32,9 @@ const CardioInput = ({onFocus, onDistanceChange, onSpeedChange, onTimeChange, on
             alignItems: 'center',
             width: '80%',   
         }}> 
+          <Ionicons name="walk-outline" size={20} color={COLORS.tertiary} style={{position:'relative', left:20}}/>  
           <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleDistanceChange} placeholder="Distance" placeholderTextColor={COLORS.gray} inputmode="Numeric" keyboardType="decimal-pad"/><Text style={[globalStyles.text, {position:'relative', right:25}]}>km</Text>
+          <Ionicons name="speedometer-outline" size={20} color={COLORS.tertiary} style={{position:'relative', left:20}}/>
           <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleSpeedChange} placeholder="Avg. Speed" inputmode="Numeric" keyboardType="decimal-pad" placeholderTextColor={COLORS.gray}/><Text style={[globalStyles.text, {position:'relative', right:25}]}>km/h</Text>
         </View>
         <View style={{
@@ -40,7 +43,9 @@ const CardioInput = ({onFocus, onDistanceChange, onSpeedChange, onTimeChange, on
             alignItems: 'center',
             width: '80%'
         }}>
+            <Ionicons name="timer-outline" size={20} color={COLORS.tertiary} style={{position:'relative', left:20}}/>
             <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleTimeChange} placeholder="Time" placeholderTextColor={COLORS.gray} inputmode="Numeric" keyboardType="decimal-pad"/><Text style={[globalStyles.text, {position:'relative', right:25}]}>min</Text>
+            <Ionicons name="cellular-outline" size={20} color={COLORS.tertiary} style={{position:'relative', left:20}}/>
             <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleElevationChange} placeholder="Elevation" inputmode="Numeric" keyboardType="decimal-pad" placeholderTextColor={COLORS.gray}/><Text style={[globalStyles.text, {position:'relative', right:25}]}>%</Text>
         </View>
         </View>
--- components/WeightRepInput.js
+++ components/WeightRepInput.js
@@ -2,6 +2,7 @@ import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import {COLORS, FONTS, SHADOWS, SIZES} from '../constants'
 import globalStyles from '../constants/GlobalStyle'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const WeightRepInput = ({number, onWeightChange, onRepChange, onFocus}) => {
   const handleWeightChange = (weight) => {
@@ -20,9 +21,16 @@ const WeightRepInput = ({number, onWeightChange, onRepChange, onFocus}) => {
         justifyContent: 'space-between',
         alignItems: 'center',
     }}>
-      <Text style={[globalStyles.text, {marginRight:30}]}>{number}</Text>  
-      <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleWeightChange} placeholder="Weight" placeholderTextColor={COLORS.gray} inputmode="Numeric" keyboardType="decimal-pad"/><Text style={[globalStyles.text, {position:'relative', right:25}]}>kg</Text>
-      <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleRepChange} placeholder="Reps" inputmode="Numeric" keyboardType="decimal-pad" placeholderTextColor={COLORS.gray} />
+      <Text style={[globalStyles.text]}>{number}.</Text>
+      
+        <Ionicons name="barbell-outline" size={20} color={COLORS.tertiary} style={{position:'relative', left:20}}/>
+        <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleWeightChange} placeholder="Weight" placeholderTextColor={COLORS.gray} inputmode="Numeric" keyboardType="decimal-pad"/><Text style={[globalStyles.text, {position:'relative', right:25}]}>kg</Text>
+
+      
+      
+        <Ionicons name="repeat-outline" size={20} color={COLORS.tertiary} style={{position:'relative', left:20}}/>
+        <TextInput style={globalStyles.smallInput} onFocus={onFocus} onChangeText={handleRepChange} placeholder="Reps" inputmode="Numeric" keyboardType="decimal-pad" placeholderTextColor={COLORS.gray} />
+      
     </View>
   )
 }
