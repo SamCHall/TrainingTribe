ca649a31fcd8febdc374f7c87462dec91eaaff6f
Samuel Hall <hallsc3@cardiff.ac.uk>
2023-09-01 17:09:12 +0100

    Added logout button to TribeChooser

--- screens/TribeChooser.js
+++ screens/TribeChooser.js
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useUser } from "@realm/react";
+import { useUser, useApp } from "@realm/react";
 import { CustomStatusBar, OvalButton } from "../components";
 import { useNavigation } from "@react-navigation/native";
 import globalStyles from "../constants/GlobalStyle";
@@ -8,20 +8,26 @@ import globalStyles from "../constants/GlobalStyle";
 const TribeChooser = () => {
   const user = useUser();
   const navigation = useNavigation();
+  const app = useApp();
 
   console.log(user.customData.tribe);
   if (!user.customData.tribe || user.customData.tribe === null) {
     return (
-      <View style={globalStyles.centeredContainer}>
-        <CustomStatusBar />
-        <OvalButton
-          text="Create a Tribe"
-          onPress={() => navigation.navigate("CreateTribe")}
-        />
-        <OvalButton
-          text="Join a Tribe"
-          onPress={() => navigation.navigate("JoinTribe")}
-        />
+      <View style={globalStyles.container}>
+        <View style={globalStyles.centeredContainer}>
+          <CustomStatusBar />
+          <OvalButton
+            text="Create a Tribe"
+            onPress={() => navigation.navigate("CreateTribe")}
+          />
+          <OvalButton
+            text="Join a Tribe"
+            onPress={() => navigation.navigate("JoinTribe")}
+          />
+        </View>
+        <View style={{ position: "absolute", bottom: 20, alignSelf: "center" }}>
+          <OvalButton text="Log Out" onPress={() => app.currentUser.logOut()} />
+        </View>
       </View>
     );
   }
